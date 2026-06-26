7871da72c0b4fd5d5d43a3dca4e4add430c7973a
annvinodjacob <45268445+annvinodjacob@users.noreply.github.com>
2026-06-26 15:02:27 +0530

    Update MyFirstPython.py

--- MyFirstPython.py
+++ MyFirstPython.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from google import genai
+config=types.GenerateContentConfig(
+  system_instructions="""You are an expert in Python programming. Answer all questions asked about Python. Any queries outside Python area, respond as given below.
+  "Kindly ask questions about Python Programming Language only".
+  """
 st.markdown(
   """
   <h1 style='text-align: center;'> Python AI Assistant</h1>
@@ -15,7 +19,7 @@ mychat = robo.chats.create(model="gemini-flash-lite-latest")
 response_placeholder = st.empty()
 
 question = st.text_input("", placeholder="Enter your Python question here...")
-
+question=config+question
 col1, col2, col3 = st.columns([4, 1, 4])
 
 with col2:
